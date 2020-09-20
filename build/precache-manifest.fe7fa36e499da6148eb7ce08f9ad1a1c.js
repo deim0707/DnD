bcc437c568ccd8cfafc1547dde6251f092af63cf
deim0707 <dima1parshin@gmail.com>
2020-09-20 19:01:29 +0300

    добавил сборку

---
+++ build/precache-manifest.fe7fa36e499da6148eb7ce08f9ad1a1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0378b4b3bd2869ea2f0b90cd92bd76c7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f6e85cf9704db183ee3",
+    "url": "./static/css/main.293b4343.chunk.css"
+  },
+  {
+    "revision": "b7ccac73d0f5565df0a6",
+    "url": "./static/js/2.b236ba74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b236ba74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6e85cf9704db183ee3",
+    "url": "./static/js/main.694b8f20.chunk.js"
+  },
+  {
+    "revision": "f45df06ee243769e16f3",
+    "url": "./static/js/runtime-main.4e95076c.js"
+  },
+  {
+    "revision": "bf499e682349309ecb772993cef435c4",
+    "url": "./static/media/asteroid.bf499e68.svg"
+  },
+  {
+    "revision": "40871027c24ecb65aca2a4d47101cbf5",
+    "url": "./static/media/leaf.40871027.svg"
+  }
+]);
